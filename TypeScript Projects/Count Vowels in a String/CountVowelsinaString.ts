93f36e2186fdc6d0a59912424db927c6b1add536
Jakub🐕 <166159581+JakubStachh@users.noreply.github.com>
2025-04-05 05:51:30 +0200

    Update CountVowelsinaString.ts

--- TypeScript Projects/Count Vowels in a String/CountVowelsinaString.ts
+++ TypeScript Projects/Count Vowels in a String/CountVowelsinaString.ts
@@ -2,4 +2,4 @@ function countVowels(str: string): number {
     return (str.match(/[aeiou]/gi) || []).length;
 }
 
-console.log(countVowels("typescript")); // Output: 3
+console.log(countVowels("AEIOU")); // Output: 5
